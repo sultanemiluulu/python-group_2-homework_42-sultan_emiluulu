eb13ba8df22f150b4a26e9244df8660fb9b85bee
sultaneu <sultanchoponov@gmail.com>
2018-12-20 15:10:14 +0600

    Creating views

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from webapp.models import Client, Article, Comment, Rating
 
-# Create your views here.
+
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'article_list.html'
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'article_detail.html'
+
+
+class ClientListView(ListView):
+    model = Client
+    template_name = 'client_list.html'
+
+
+class ClientDetailView(DetailView):
+    model = Client
+    template_name = 'client_detail.html'
