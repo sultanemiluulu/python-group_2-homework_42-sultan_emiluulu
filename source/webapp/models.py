b5461b71f989e19e1285431e3e49abe538240264
sultaneu <sultanchoponov@gmail.com>
2018-12-20 13:51:04 +0600

    Created model "Client"

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Client(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False, verbose_name='Client name')
+    email = models.EmailField(max_length=100, null=True, blank=True, verbose_name='Email')
+    password = models.CharField(max_length=16, null=False, blank=False, verbose_name='Password')
+
+    def __str__(self):
+        return self.name
